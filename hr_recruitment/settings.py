04ce942be9d315f9ceb95c656b5149bdb2495326
Atiq Umer <atiqumer15@gmail.com>
2025-10-30 08:48:12 +0500

    Next page issue

--- hr_recruitment/settings.py
+++ hr_recruitment/settings.py
@@ -37,13 +37,10 @@ else:
 CORS_ALLOW_ALL_ORIGINS = True 
 
 # If CORS_ALLOW_ALL_ORIGINS = False, this list is used:
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "https://neuralhirefrontend.netlify.app", 
-# ]
-
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://neuralhirefrontend.netlify.app", 
+]
 CORS_ALLOW_HEADERS = [
     "authorization",
     "content-type",
@@ -52,6 +49,10 @@ CORS_ALLOW_HEADERS = [
     "user-agent",
     "x-requested-with",
 ]
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 X_FRAME_OPTIONS = 'ALLOWALL'
 ROOT_URLCONF = 'hr_recruitment.urls'
 
